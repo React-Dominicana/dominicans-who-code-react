fbf085ff5ec8d487829499085dd837fa475564f5
Giancarlos Castillo <gcastillo@mbp.local>
2019-09-08 17:18:45 -0400

    fix: remove unused import

--- src/components/CoderCard/index.js
+++ src/components/CoderCard/index.js
@@ -6,12 +6,12 @@ import CardTitle from 'reactstrap/lib/CardTitle'
 import CardSubtitle from 'reactstrap/lib/CardSubtitle'
 import CardText from 'reactstrap/lib/CardText'
 import CardBody from 'reactstrap/lib/CardBody'
-
 const CoderCard = memo((props) => {
+
   return (
     <Col xs={12} md={4}>
       <Card className="m-3">
-        <CardImg top  src={props.image} alt="A dominican coder" />
+        <CardImg top src={props.image} alt={`${props.name}. ${props.summary}. He knows ${props.skills}`} />
         <CardBody>
           <CardTitle>{props.name}</CardTitle>
           <CardSubtitle>{props.initials}</CardSubtitle>
