061ebb53c5e72e67e28fd3f5244e3a0b3140119d
Manuel Cepeda <mecm1993@gmail.com>
2019-09-08 19:37:49 -0400

    Randomize list and minor changes in card style

--- src/components/CoderCard/index.js
+++ src/components/CoderCard/index.js
@@ -1,7 +1,6 @@
 import React, { memo } from 'react'
 import Col from 'reactstrap/lib/Col'
 import Card from 'reactstrap/lib/Card'
-import CardImg from 'reactstrap/lib/CardImg'
 import CardTitle from 'reactstrap/lib/CardTitle'
 import CardSubtitle from 'reactstrap/lib/CardSubtitle'
 import CardText from 'reactstrap/lib/CardText'
@@ -11,16 +10,24 @@ import Badge from 'reactstrap/lib/Badge'
 const CoderCard = memo((props) => {
   const skills = props.skills.split(',')
 
-  const renderSkillsTag = (skill, index) => 
-    <Badge color="primary" key={index}>{skill}</Badge>
+  const renderSkillsTag = (skill, index) =>
+    <Badge color="primary" key={index} style={{ margin: '0 2px' }}>{skill}</Badge>
 
   return (
     <Col xs={12} md={4}>
       <Card className="m-3">
-        <CardImg top src={props.image} alt={`${props.name}. ${props.summary}. He knows ${props.skills}`} />
+        <div
+          style={{
+            backgroundColor: '#212529',
+            backgroundImage: `url(${props.image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'top',
+            height: '300px'
+          }} />
         <CardBody>
           {skills.map(renderSkillsTag)}
-          <CardTitle>{props.name}</CardTitle>
+          <CardTitle style={{ fontSize: '20px', fontWeight: '800' }}>{props.name}</CardTitle>
           <CardSubtitle>{props.initials}</CardSubtitle>
           <CardText>{props.summary}</CardText>
         </CardBody>
--- src/components/DominicansWhoCodesList/index.js
+++ src/components/DominicansWhoCodesList/index.js
@@ -8,11 +8,25 @@ import useDominicanCoders from '../../hooks/useDominicanCoders'
 const DominicansWhoCodesList = () => {
     const { data, loading } = useDominicanCoders()
 
+    const shuffle = (array) => {
+        var currentIndex = array.length, temporaryValue, randomIndex
+
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex)
+          currentIndex -= 1
+          temporaryValue = array[currentIndex]
+          array[currentIndex] = array[randomIndex]
+          array[randomIndex] = temporaryValue
+        }
+      
+        return array
+    }
+
     const renderCodersCards = (coder, index) =>
         <CoderCard  {...coder} key={index} />
 
     return loading ? 'Loading'
-        : (<Row>{data.map(renderCodersCards)}</Row>)
+        : (<Row>{shuffle(data).map(renderCodersCards)}</Row>)
 }
 
 export default DominicansWhoCodesList
