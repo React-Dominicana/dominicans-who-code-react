e85dae92a5a9421a0677528159a159cb10edb1b9
Manuel Cepeda <mecm1993@gmail.com>
2019-09-08 23:50:28 -0400

    Fix due to missing import

--- src/hooks/useDominicanCoders.js
+++ src/hooks/useDominicanCoders.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { checkUrl } from '../utils'
+import * as helper from '../utils/helper'
 
 const buildUrl = criteria =>
     `${process.env.REACT_APP_API_URL}${criteria ? `/${criteria}` : ''}`
