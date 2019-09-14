94a3dc570571903849fb362a88d882661e54a264
Giancarlos Castillo <gcastillo@mbp.local>
2019-09-14 19:30:06 -0400

    improves: refactor pathOr
    since obj[criteria] will return undefined even if the object is empty, the validation is redundant. however, we keep isEmptyObject function in case we can use it in other part

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,24 +21,18 @@ export const checkUrl = word => {
 export const isEmptyObject = obj =>
     (Object.entries(obj).length === 0 && obj.constructor === Object)
 
-
 /**
  * Search for a property inside an object.
  * 
- * @param {string} defaultValue the value we set if the object property is undefined or null
- * @param {*} criteria the property we're searching for
+ * @param {string} dflt the value we set if the object property is undefined or null
+ * @param {*} c the property we're searching for
  * @param {*} obj the object where we want to search 
  * 
  * @returns {defaultValue | obj[criteria]} the default value we passed or the property value we're searching for.
  */
-export const pathOr = (defaultValue = '', criteria = '', obj) => {
-    if(isEmptyObject(obj))
-        return defaultValue
-    else if(!obj[criteria]) 
-        return defaultValue
-    else
-        return obj[criteria]
-}
+export const pathOr = (defaultValue = '', criteria = '', obj) =>
+    obj[criteria] || defaultValue
+
 
 export const randomize = () =>
     0.5 - Math.random()
